82a9bb514806c370fc2284d34247fc64112cd8c3
Wesley <wesleysantos.ramos.ws@gmail.com>
2018-09-20 16:46:06 -0300

    foi?

--- RP2/src/Cronos.java
+++ RP2/src/Cronos.java
@@ -2,7 +2,7 @@
 public class Cronos {
 	
 	/*Classe que controla o tempo e as entidades que o tempo influencia*/
-	
+	// Este comentario esta uma maravilha
 	int qViagens =0; //quantidade de viagens realizadas
 	float totalTempo=0; //variavel que acumula o tempo de todas as viagens
 	
