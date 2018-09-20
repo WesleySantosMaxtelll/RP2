32d76f06548935ee95a698f83c709638879a9074
Wesley <wesleysantos.ramos.ws@gmail.com>
2018-09-20 17:19:20 -0300

    Definição dos parametros das classes

--- RP2/src/itens/Pessoa.java
+++ RP2/src/itens/Pessoa.java
@@ -2,13 +2,41 @@ package itens;
 
 public class Pessoa {
 	
-	int destino=0;
-	float inicioEspera=0;// em que tempo(horario a pessoa chegou ao ponto)
-	
-	public Pessoa(int pontoAtual){
-		
-		destino= 0;// tem que ser um ponto depois do ponto atual;
-		inicioEspera=Cronos.tempoUniversal;// come�ou a espera;
+	private int destino = 0;
+	private int partida = 0;
+	private float inicioEspera = 0;// em que tempo(horario a pessoa chegou ao ponto)
+	private float horarioChegada = 0;
+	public Pessoa(int destino, int partida, float inicioEspera) {
+		super();
+		this.destino = destino;
+		this.partida = partida;
+		this.inicioEspera = inicioEspera;
 	}
+	public int getDestino() {
+		return destino;
+	}
+	public void setDestino(int destino) {
+		this.destino = destino;
+	}
+	public int getPartida() {
+		return partida;
+	}
+	public void setPartida(int partida) {
+		this.partida = partida;
+	}
+	public float getInicioEspera() {
+		return inicioEspera;
+	}
+	public void setInicioEspera(float inicioEspera) {
+		this.inicioEspera = inicioEspera;
+	}
+	public float getHorarioChegada() {
+		return horarioChegada;
+	}
+	public void setHorarioChegada(float horarioChegada) {
+		this.horarioChegada = horarioChegada;
+	}
+	
+	
 
 }
